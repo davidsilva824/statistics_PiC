c41fd10b31bb94747b69574b6b33e552f6e0765c
davidsilva824 <davidsilva824@hotmail.com>
2026-02-28 14:07:35 +0000

    Commit 12

---
+++ Cross-Model Analysis_treatment_contrast.R
@@ -0,0 +1,232 @@
+library(lme4)
+library(lmerTest)
+library(dplyr)
+library(stringr)
+
+# this folder contains the results files obtained from study_pic_4: https://github.com/davidsilva824/study_pic_4 
+setwd("C:/Users/Admin/Desktop/Dissertação/código/satistics_PiC/Statistics_PiC/results_experiment_1")
+
+
+# To read all files in a folder:
+# file_list <- list.files(pattern="\\.csv$", full.names = FALSE)
+
+
+# To read one or more specific files in the folder:
+file_list <- c(
+  "results_experiment_1_babyLlama_100M.csv",
+  "results_experiment_1_babylm__opt-125m-strict-2023.csv",
+  "results_experiment_1_BabyLM-community__babylm-baseline-100m-gpt-bert-causal-focus.csv",
+  "results_experiment_1_colinglab__CLASS_IT-140M.csv",
+  "results_experiment_1_gpt_2_100M.csv",
+  "results_experiment_1_phonemetransformers__GPT2-85M-BPE-TXT.csv",
+  "results_experiment_1_bbunzeck__grapheme-llama.csv",
+  "results_experiment_1_BabyLM-community__babylm-baseline-100m-gpt-bert-masked-focus.csv",
+  "results_experiment_1_BabyLM-community__babylm-baseline-100m-gpt-bert-mixed.csv",
+  "results_experiment_1_phonemetransformers__GPT2-85M-CHAR-TXT.csv"
+)
+
+# -------------------------------------------------------------------------------------------
+
+### FUNCTIONS
+
+# a function to check if a model converges 
+hasConverged <- function(mm) {
+  if (is.null(unlist(mm@optinfo$conv$lme4))) { # returns 1 if mm@optinfo$conv$lme4 is NULL, meaning no convergence messages/warnings.
+    return(1)
+  } else {
+    if (isSingular(mm)) return(0) #returns 0 if the fit is singular.
+    return(-1) # returns -1 for other convergence problems.
+  }
+}
+
+
+# Function to fit the fit mixed models.
+# here I need to use tryCatch to prevent the script from crashing if an error happens, in order to try all the models to completion. 
+# tryCatch tries the code in the first block. If that code works, it returns its result.
+# If it throws an error, it jumps to error = function(e) where it is defined what to return: AIC = NA, convergence = -99, and the error message.
+
+fit_model <- function(fml, current_dat){
+  out <- tryCatch({
+    m <- lmer(fml, current_dat, REML = TRUE)
+    list(
+      AIC = as.numeric(AIC(m)),
+      convergence = hasConverged(m),
+      error = NA_character_
+    )
+  }, error = function(e){
+    list(
+      AIC = NA_real_,
+      convergence = -99,
+      error = conditionMessage(e)
+    )
+  })
+  out
+}
+
+# function to add "Surprisal.head ~ 1 + regularity * plurality + " to all the possible combinations in the formula
+# |> passes the previous result to the next function. 
+# sapply() often gives the result names taken from the input vector. unname() removes them, returning just the formulas.
+
+build_formulas <- function(re_strings){ # Creates a function called build_formulas
+  sapply( # sapply() applies a function to each element of a vector/list
+    re_strings,
+    \(x) as.formula(paste0("Surprisal.head ~ 1 + regularity * plurality + ", x)) # \(x) is the way to write an anonimous function in R.Similar to lambda in python.
+  ) |> unname() 
+}
+
+# This function fits all the formulas and returns a results table.
+fit_formula_list <- function(formulas, current_dat){
+  rows <- vector("list", length(formulas)) # 
+  for(i in seq_along(formulas)){
+    res <- fit_model(formulas[[i]], current_dat)
+    clean_formula <- paste(deparse(formulas[[i]], width.cutoff = 500), collapse = " ")
+    rows[[i]] <- data.frame(
+      AIC = res$AIC,
+      convergence = res$convergence,
+      formula = clean_formula,
+      error = res$error,
+      stringsAsFactors = FALSE
+    )
+  }
+  do.call(rbind, rows)
+}
+
+# function to convert the file name into the model name. 
+get_model_name <- function(fn){
+  x <- fn
+  x <- str_replace(x, "\\.csv$", "")
+  x <- str_replace(x, "^results_experiment_\\d+_", "")
+  x
+}
+
+# ------------------------------------------------------------
+
+### Loading and stacking the information in the results files into a dataframe.
+
+# Creating the dataframe with all the variables. 
+cat("Found", length(file_list), "files to process.\n\n")
+
+dat_all <- lapply(file_list, function(f){
+  d <- read.csv(f, check.names = TRUE)
+  d$model <- get_model_name(f)
+  d$File <- f
+  d
+}) |> bind_rows()
+
+# sanity check: to check if the dat_all has all the colmuns needed for models to run. 
+needed <- c("Surprisal.head","Category","Non.Head","Head","model")
+missing <- setdiff(needed, names(dat_all))
+if(length(missing) > 0) stop(paste("Missing columns:", paste(missing, collapse=", ")))
+
+# ------------------------------------------------------------
+
+### Data preparation.
+dat_all <- dat_all %>%
+  mutate(
+    regularity = ifelse(grepl("Irregular", Category), "Irregular", "Regular"),
+    plurality  = ifelse(grepl("Plural", Category), "Plural", "Singular"),
+    regularity = factor(regularity, levels = c("Regular", "Irregular")),
+    plurality  = factor(plurality,  levels = c("Plural", "Singular")),
+    model      = factor(model)
+  )
+
+dat_all$regularity_num <- ifelse(dat_all$regularity == "Regular", 0, 1)
+dat_all$plurality_num  <- ifelse(dat_all$plurality  == "Plural", 0, 1)
+
+dat_all <- dat_all %>%
+  mutate(
+    set = case_when(
+      Non.Head %in% c("goose","geese","swan","swans") ~ "set_goose_swan",
+      Non.Head %in% c("ox","oxen","cow","cows") ~ "set_ox_cow",
+      Non.Head %in% c("louse","lice","flea","fleas") ~ "set_louse_flea",
+      Non.Head %in% c("mouse","mice","rat","rats") ~ "set_mouse_rat",
+      Non.Head %in% c("foot","feet","leg","legs") ~ "set_foot_leg",
+      Non.Head %in% c("tooth","teeth","bone","bones") ~ "set_tooth_bone",
+      Non.Head %in% c("child","children","adult","adults") ~ "set_child_adult",
+      Non.Head %in% c("woman","women","girl","girls") ~ "set_woman_girl",
+      Non.Head %in% c("man","men","boy","boys") ~ "set_man_boy",
+      Non.Head %in% c("salesman","salesmen","retailer","retailers") ~ "set_salesman_retailer",
+      Non.Head %in% c("nobleman","noblemen","aristocrat","aristocrats") ~ "set_nobleman_aristocrat",
+      Non.Head %in% c("boatman","boatmen","shipmate","shipmates") ~ "set_boatman_shipmate",
+      Non.Head %in% c("craftsman","craftsmen","labourer","labourers") ~ "set_craftsman_labourer",
+      Non.Head %in% c("fireman","firemen","lifeguard","lifeguards") ~ "set_fireman_lifeguard",
+      TRUE ~ NA_character_
+    )
+  )
+
+# If any NA sets exist, drop them.
+dat_all <- dat_all %>% filter(!is.na(set))
+
+# ------------------------------------------------------------
+
+# Building the random-effects terms.
+
+# Slope menu (factor-coded, correlated)
+rs <- c("",
+        " + regularity",
+        " + plurality",
+        " + regularity + plurality",
+        " + regularity * plurality")
+
+rs_model <- sapply(rs, \(x) paste0("(1", x, " | model)")) |> unname()
+rs_set   <- sapply(rs, \(x) paste0("(1", x, " | set)"))   |> unname()
+rs_head  <- sapply(rs, \(x) paste0("(1", x, " | Head)"))  |> unname()
+
+# Numeric (uncorrelated) options
+rs_num <- c(" + regularity_num",
+            " + plurality_num",
+            " + regularity_num + plurality_num",
+            " + regularity_num * plurality_num")
+
+rs_model_uncorr <- c("(1 | model)", sapply(rs_num, \(x) paste0("(1", x, " || model)")) |> unname())
+rs_set_uncorr   <- c("(1 | set)",   sapply(rs_num, \(x) paste0("(1", x, " || set)"))   |> unname())
+rs_head_uncorr  <- c("(1 | Head)",  sapply(rs_num, \(x) paste0("(1", x, " || Head)"))  |> unname())
+
+# ------------------------------------------------------------
+
+### Generating and fitting all the combinations for the results of all the models. 
+
+# G1: all correlated
+re_g1 <- apply(expand.grid(rs_model, rs_set, rs_head), 1, \(x) paste(x, collapse=" + "))
+fr_g1 <- build_formulas(re_g1)
+df_g1 <- fit_formula_list(fr_g1, dat_all)
+
+# G2: all uncorrelated
+re_g2 <- apply(expand.grid(rs_model_uncorr, rs_set_uncorr, rs_head_uncorr), 1, \(x) paste(x, collapse=" + "))
+re_g2 <- re_g2[re_g2 != "(1 | model) + (1 | set) + (1 | Head)"]
+fr_g2 <- build_formulas(re_g2)
+df_g2 <- fit_formula_list(fr_g2, dat_all)
+
+# G3: model correlated, set/head uncorrelated
+re_g3 <- apply(expand.grid(rs_model, rs_set_uncorr, rs_head_uncorr), 1, \(x) paste(x, collapse=" + "))
+re_g3 <- re_g3[re_g3 != "(1 | model) + (1 | set) + (1 | Head)"]
+fr_g3 <- build_formulas(re_g3)
+df_g3 <- fit_formula_list(fr_g3, dat_all)
+
+# G4: model uncorrelated, set/head correlated
+re_g4 <- apply(expand.grid(rs_model_uncorr, rs_set, rs_head), 1, \(x) paste(x, collapse=" + "))
+re_g4 <- re_g4[re_g4 != "(1 | model) + (1 | set) + (1 | Head)"]
+fr_g4 <- build_formulas(re_g4)
+df_g4 <- fit_formula_list(fr_g4, dat_all)
+
+all_results <- rbind(df_g1, df_g2, df_g3, df_g4)
+
+# pick winner
+winner <- all_results[all_results$convergence == 1, ]
+if(nrow(winner) == 0) stop("No models converged in the global fit.")
+winner <- winner[which.min(winner$AIC), ]
+
+cat("\n--- GLOBAL MODEL SELECTION COMPLETE ---\n\n")
+print(winner[, c("formula","AIC")], row.names = FALSE)
+
+# ------------------------------------------------------------
+
+### Code to test the winner. Must copy paste manually.
+
+final_model <- lmer(
+  Surprisal.head ~ 1 + regularity / plurality +  (1 | model) + (1 + regularity_num + plurality_num || set) + (1 + plurality_num || Head), 
+  data = dat_all, 
+  REML = TRUE
+)
+
+summary(final_model)
---
+++ running_Cross_model_analysis.R
@@ -0,0 +1,96 @@
+library(lme4)
+library(lmerTest)
+library(dplyr)
+library(stringr)
+
+setwd("C:/Users/Admin/Desktop/Dissertação/código/satistics_PiC/Statistics_PiC/results_experiment_1")
+
+file_list <- c(
+  "results_experiment_1_babyLlama_100M.csv",
+  "results_experiment_1_babylm__opt-125m-strict-2023.csv",
+  "results_experiment_1_BabyLM-community__babylm-baseline-100m-gpt-bert-causal-focus.csv",
+  "results_experiment_1_colinglab__CLASS_IT-140M.csv",
+  "results_experiment_1_gpt_2_100M.csv",
+  "results_experiment_1_phonemetransformers__GPT2-85M-BPE-TXT.csv"
+)
+
+get_model_name <- function(fn){
+  x <- fn
+  x <- str_replace(x, "\\.csv$", "")
+  x <- str_replace(x, "^results_experiment_\\d+_", "")
+  x
+}
+
+dat_all <- lapply(file_list, function(f){
+  d <- read.csv(f, check.names = TRUE)
+  d$model <- get_model_name(f)
+  d$File <- f
+  d
+}) |> bind_rows()
+
+dat_all <- dat_all %>%
+  mutate(
+    regularity = ifelse(grepl("Irregular", Category), "Irregular", "Regular"),
+    plurality  = ifelse(grepl("Plural", Category), "Plural", "Singular"),
+    regularity = factor(regularity, levels = c("Regular","Irregular")),
+    plurality  = factor(plurality,  levels = c("Plural","Singular")),
+    model      = factor(model),
+    regularity_num = ifelse(regularity == "Regular", 0, 1),
+    plurality_num  = ifelse(plurality  == "Plural", 0, 1),
+    set = case_when(
+      Non.Head %in% c("goose","geese","swan","swans") ~ "set_goose_swan",
+      Non.Head %in% c("ox","oxen","cow","cows") ~ "set_ox_cow",
+      Non.Head %in% c("louse","lice","flea","fleas") ~ "set_louse_flea",
+      Non.Head %in% c("mouse","mice","rat","rats") ~ "set_mouse_rat",
+      Non.Head %in% c("foot","feet","leg","legs") ~ "set_foot_leg",
+      Non.Head %in% c("tooth","teeth","bone","bones") ~ "set_tooth_bone",
+      Non.Head %in% c("child","children","adult","adults") ~ "set_child_adult",
+      Non.Head %in% c("woman","women","girl","girls") ~ "set_woman_girl",
+      Non.Head %in% c("man","men","boy","boys") ~ "set_man_boy",
+      Non.Head %in% c("salesman","salesmen","retailer","retailers") ~ "set_salesman_retailer",
+      Non.Head %in% c("nobleman","noblemen","aristocrat","aristocrats") ~ "set_nobleman_aristocrat",
+      Non.Head %in% c("boatman","boatmen","shipmate","shipmates") ~ "set_boatman_shipmate",
+      Non.Head %in% c("craftsman","craftsmen","labourer","labourers") ~ "set_craftsman_labourer",
+      Non.Head %in% c("fireman","firemen","lifeguard","lifeguards") ~ "set_fireman_lifeguard",
+      TRUE ~ NA_character_
+    )
+  ) %>%
+  filter(!is.na(set))
+
+
+out_file <- "global_model_pluralref_vs_singref.txt"
+sink(out_file)
+
+cat("Global model results (Plural ref vs Singular ref)\n")
+cat("Generated:", format(Sys.time()), "\n\n")
+
+# --- PLURAL reference ---
+dat_plur <- dat_all %>% mutate(plurality = factor(plurality, levels = c("Plural","Singular")))
+
+final_model_plurref <- lmer(
+  Surprisal.head ~ 1 + regularity * plurality +
+    (1 | model) + (1 + plurality | set) + (1 + plurality | Head),
+  data = dat_plur,
+  REML = TRUE
+)
+
+cat("\n--- PLURAL reference ---\n")
+print(summary(final_model_plurref))
+cat("isSingular:", isSingular(final_model_plurref), "\n\n")
+
+# --- SINGULAR reference ---
+dat_sing <- dat_all %>% mutate(plurality = factor(plurality, levels = c("Singular","Plural")))
+
+final_model_singref <- lmer(
+  Surprisal.head ~ 1 + regularity * plurality +
+    (1 | model) + (1 + plurality | set) + (1 + plurality | Head),
+  data = dat_sing,
+  REML = TRUE
+)
+
+cat("\n--- SINGULAR reference ---\n")
+print(summary(final_model_singref))
+cat("isSingular:", isSingular(final_model_singref), "\n\n")
+
+sink()
+cat("Wrote:", out_file, "\n")
